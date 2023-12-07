2394f25053955fa57c589b4476753f9d76aa9062
Gerard Whittey <gerard.whittey@gmail.com>
2023-12-07 18:02:11 -0500

    Fix bug in isUrlValid

--- app/src/main/java/org/maddiesoftware/komagareader/server_select/domain/use_case/ValidateUrl.kt
+++ app/src/main/java/org/maddiesoftware/komagareader/server_select/domain/use_case/ValidateUrl.kt
@@ -1,5 +1,6 @@
 package org.maddiesoftware.komagareader.server_select.domain.use_case
 
+import android.util.Log
 import android.util.Patterns
 import android.webkit.URLUtil
 
@@ -17,7 +18,8 @@ class ValidateUrl {
                 errorMessage = "Url must end with a /"
             )
         }
-        if(isUrlValid(url)){
+        if(! isUrlValid(url)){
+            Log.d("Komga",url)
             return ValidationResult(
                 success = false,
                 errorMessage = "Url not valid/"
